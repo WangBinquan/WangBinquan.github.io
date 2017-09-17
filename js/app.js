14852446b5ec48e112716ee039e95e55046e6fb6
BinquanWang <binquanwang.wbq@gmail.com>
2017-09-17 14:45:53 +0800

    ..

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+(function init() {
+    window.onresize = function(){
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+        $("#main-body-id").height(height)
+
+    }
+})()
